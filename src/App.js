3ccd9cde3027fba86f4a1f1239c1ad90b36d45ac
karthik <ms18karthik@gmail.com>
2019-07-15 22:35:47 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet),
+    React.createElement(Pet),
+    React.createElement(Pet)
+  ]);
+};
+
+render(React.createElement(App), document.getElementById("root"));
