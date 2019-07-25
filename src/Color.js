197364ce2a550d3ff72eedb385091a6d7e4698a8
karthik <ms18karthik@gmail.com>
2019-07-25 15:22:58 +0530

    making design system and animations

---
+++ src/Color.js
@@ -0,0 +1,6 @@
+export default {
+  primary: "#365FFF",
+  secondary: "#36BBFF",
+  dark: "#F87060",
+  light: "#B3A394"
+};
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,18 +1,37 @@
 import React from "react";
 import { Link } from "@reach/router";
-import { css } from "@emotion/core";
+import { css, keyframes } from "@emotion/core";
+import Color from "./Color";
+
+const spin = keyframes`
+  to {
+    transform: rotate(360deg);
+  }
+`;
 
 const NavBar = () => (
   <header
     css={css`
-      background-color: #333;
+      background-color: ${Color.dark};
       position: sticky;
       top: 0;
       z-index: 10;
     `}
   >
     <Link to="/">Adopt Me!</Link>
-    <span aria-label="logo" role="img">
+    <span
+      css={css`
+        font-size: 60px;
+        display: inline-block;
+        animation: 1s ${spin} linear infinite;
+        &:hover {
+          animation: 1s ${spin} reverse infinite;
+          text-decoration: underline;
+        }
+      `}
+      aria-label="logo"
+      role="img"
+    >
       🐩
     </span>
   </header>
